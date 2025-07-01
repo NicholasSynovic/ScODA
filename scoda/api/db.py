65003a3a8271147cc4780ebea823264d5293b246
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-01 11:12:23 -0500

    Update application to write multiple datasets to individual tables

--- scoda/api/db.py
+++ scoda/api/db.py
@@ -18,4 +18,44 @@ class DB:
             Column("measured_kW", Integer),
         )
 
+        _: Table = Table(
+            "hawk_power_15_min",
+            self.metadata,
+            Column("id", Integer, primary_key=True),
+            Column("timestamp_secs", Integer),
+            Column("measured_kW", Integer),
+        )
+
+        _: Table = Table(
+            "lumi_power_10_min",
+            self.metadata,
+            Column("id", Integer, primary_key=True),
+            Column("timestamp_secs", Integer),
+            Column("measured_kW", Integer),
+        )
+
+        _: Table = Table(
+            "marconi100_power_60_sec",
+            self.metadata,
+            Column("id", Integer, primary_key=True),
+            Column("timestamp_secs", Integer),
+            Column("measured_kW", Integer),
+        )
+
+        _: Table = Table(
+            "perlmutter_power_60_sec",
+            self.metadata,
+            Column("id", Integer, primary_key=True),
+            Column("timestamp_secs", Integer),
+            Column("measured_kW", Integer),
+        )
+
+        _: Table = Table(
+            "lumi_hpcg",
+            self.metadata,
+            Column("id", Integer, primary_key=True),
+            Column("timestamp_secs", Integer),
+            Column("measured_kW", Integer),
+        )
+
         self.metadata.create_all(bind=self.engine, checkfirst=True)
--- scoda/app.py
+++ scoda/app.py
@@ -3,11 +3,26 @@ from scoda.api.db import DB
 import pandas as pd
 from pandas import DataFrame
 from pathlib import Path
+from collections import defaultdict
 
 DATA_DIR: Path = Path(
     Path(__file__).parent.parent.resolve(),
     "LAST/Power-Provisioning-Dataset",
 )
+
+DATASET_PATHS: dict[str, Path] = {
+    "cori_power_30_sec": Path(DATA_DIR, "Cori_power_30_sec.csv"),
+    "hawk_power_15_min": Path(DATA_DIR, "Hawk_power_15_min.csv"),
+    "lumi_power_10_min": Path(DATA_DIR, "Lumi_power_10_min.csv"),
+    "marconi100_power_60_sec": Path(DATA_DIR, "Marconi100_power_60_sec.csv"),
+    "perlmutter_power_60_sec": Path(DATA_DIR, "Perlmutter_power_60_sec.csv"),
+    "lumi_hpcg": Path(DATA_DIR, "lumi_hpcg_data/lumi_hpcg.csv"),
+    "hpcg_dpc": Path(DATA_DIR, "hlrs_hpl_hpcg_data/hpcg_dpc.csv"),
+    "hpcg_uc": Path(DATA_DIR, "hlrs_hpl_hpcg_data/hpcg_uc.csv"),
+    "hpl_dpc": Path(DATA_DIR, "hlrs_hpl_hpcg_data/hpl_dpc.csv"),
+    "hpl_spc": Path(DATA_DIR, "hlrs_hpl_hpcg_data/hpl_spc.csv"),
+    "hpl_uc": Path(DATA_DIR, "hlrs_hpl_hpcg_data/hpl_uc.csv"),
+}
 
 
 def initialize_state() -> None:
@@ -17,15 +32,8 @@ def initialize_state() -> None:
             "postgresql+psycopg2://admin:example@localhost:5432/research",
         ]
 
-    # Manage dataset paths as a dict formatted {NAME, PATH}
-    if "dataset_paths" not in st.session_state:
-        st.session_state["dataset_paths"] = {
-            "cori_power_30_sec": Path(DATA_DIR, "Cori_power_30_sec.csv")
-        }
-    if "dataset_df" not in st.session_state:
-        st.session_state["dataset_df"] = DataFrame()
-    if "dataset_name" not in st.session_state:
-        st.session_state["dataset_name"] = ""
+    if "dataset_dfs" not in st.session_state:
+        st.session_state["dataset_dfs"] = defaultdict(DataFrame)
 
     # Manage database connection
     if "db" not in st.session_state:
@@ -70,36 +78,36 @@ def main() -> None:
         st.toast(body=f"Connected to: {database_uri}")
     st.divider()
 
-    # 1. Read data into memory
-    st.markdown(body="## Read data into memory")
-    dataset_selection: str = st.selectbox(
-        label="Select Dataset",
-        options=st.session_state["dataset_paths"].keys(),
-    )
-    if st.button(label="Read Dataset", disabled=False):
-        st.toast(body=f"Reading: {database_uri}")
-        st.session_state["dataset_name"] = dataset_selection
-        st.session_state["dataset_df"] = pd.read_csv(
-            filepath_or_buffer=st.session_state["dataset_paths"][dataset_selection],
-        )
-        st.toast(body=f"Read dataset into memory")
+    # 1. Read datasets into memory
+    st.markdown(body="## Read datasets into memory")
+    if st.button(label="Read Datasets", disabled=False):
+        for name, fp in DATASET_PATHS.items():
+            st.session_state["dataset_dfs"][name] = pd.read_csv(
+                filepath_or_buffer=fp,
+            )
+        st.toast(body="Read datasets into memory")
+
     st.divider()
 
     # 2. Benchmark writing to database
     st.markdown(body="Benchmark writing data to the database")
     if st.button(
         label="Write data to the database",
-        disabled=st.session_state["dataset_df"].empty,
+        disabled=len(st.session_state["dataset_dfs"].items()) < 1,
     ):
-        st.toast(body=f"Writing to database")
-        st.session_state["dataset_df"].to_sql(
-            name=st.session_state["dataset_name"],
-            con=st.session_state["db"].engine,
-            if_exists="append",
-            index=False,
-        )
+        st.toast(body="Writing to database")
 
-        st.toast(body=f"Wrote to database")
+        df_name: str
+        df: DataFrame
+        for df_name, df in st.session_state["dataset_dfs"]:
+            df.to_sql(
+                name=df_name,
+                con=st.session_state["db"].engine,
+                if_exists="append",
+                index=False,
+            )
+
+        st.toast(body="Wrote to database")
 
 
 if __name__ == "__main__":

a0bc12efc4e6b687dd9ffd41c75e085a500397f8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-09 12:30:30 -0500

    Add llnl last dataset heuristics

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ from pandas import DataFrame
 from pathlib import Path
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 import seaborn as sns
 
 write_all_tables_df: DataFrame = DataFrame()
@@ -14,6 +15,65 @@ benchmark_results: list[Path] = [
     Path("data/sqlite3_10.sqlite3").resolve(),
     Path("data/sqlite3-memory_10.sqlite3").resolve(),
 ]
+
+
+def plot_table_size() -> None:
+    data: dict[str, list[str | int | float]] = {
+        "dataset": [],
+        "records": [],
+        "filesize_kb": [],
+    }
+
+    csv_files: list[Path] = [
+        Path("LAST/Power-Provisioning-Dataset/Cori_power_30_sec.csv").resolve(),
+        Path("LAST/Power-Provisioning-Dataset/Hawk_power_15_min.csv").resolve(),
+        Path("LAST/Power-Provisioning-Dataset/Lumi_power_10_min.csv").resolve(),
+        Path("LAST/Power-Provisioning-Dataset/Marconi100_power_60_sec.csv").resolve(),
+        Path("LAST/Power-Provisioning-Dataset/Perlmutter_power_60_sec.csv").resolve(),
+        Path("LAST/Power-Provisioning-Dataset/lumi_hpcg_data/lumi_hpcg.csv"),
+        Path("LAST/Power-Provisioning-Dataset/hlrs_hpl_hpcg_data/hpl_uc.csv"),
+        Path("LAST/Power-Provisioning-Dataset/hlrs_hpl_hpcg_data/hpl_spc.csv"),
+        Path("LAST/Power-Provisioning-Dataset/hlrs_hpl_hpcg_data/hpl_dpc.csv"),
+        Path("LAST/Power-Provisioning-Dataset/hlrs_hpl_hpcg_data/hpcg_uc.csv"),
+        Path("LAST/Power-Provisioning-Dataset/hlrs_hpl_hpcg_data/hpcg_dpc.csv"),
+        Path("LAST/Power-Provisioning-Dataset/hlrs_hpl_hpcg_data/hpcg_spc.csv"),
+    ]
+
+    dfs: list[DataFrame] = [
+        pd.read_csv(filepath_or_buffer=csv_fp) for csv_fp in csv_files
+    ]
+
+    idx: int
+    for idx in range(len(dfs)):
+        df: DataFrame = dfs[idx]
+        name: str = csv_files[idx].name
+
+        data["dataset"].append(idx)
+        data["records"].append(df.shape[0])
+        data["filesize_kb"].append(csv_files[idx].stat().st_size / 1024)
+
+    df: DataFrame = DataFrame(data=data)
+
+    axes: list[Axes]
+    _, axes = plt.subplots(nrows=1, ncols=2)
+    sns.barplot(data=df, x="dataset", y="records", ax=axes[0])
+    axes[0].set_title(label="Records per Dataset")
+    axes[0].set_xlabel(xlabel="Dataset")
+    axes[0].set_ylabel(ylabel="Records (log-scaled)")
+    axes[0].set_yscale(value="log")
+
+    sns.barplot(data=df, x="dataset", y="filesize_kb", ax=axes[1])
+    axes[1].set_title(label="File Size per Dataset")
+    axes[1].set_xlabel(xlabel="Dataset")
+    axes[1].set_ylabel(ylabel="File Size (kb)")
+
+    plt.suptitle(t="LLNL/LAST Dataset Hueristics", fontsize=16)
+    plt.tight_layout()
+    plt.savefig("llnl-last-dataset-heuristics.png")
+    plt.clf()
+    plt.close(fig="all")
+
+    pass
 
 
 def plot_benchmark_1() -> None:
@@ -62,4 +122,5 @@ def plot_benchmark_2() -> None:
     pass
 
 
+plot_table_size()
 plot_benchmark_1()

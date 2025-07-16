327068f66e223d71a3946f8eda175f73405f0d00
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-16 10:06:38 -0500

    Add docstrings

--- scoda/db/__init__.py
+++ scoda/db/__init__.py
@@ -1,29 +1,97 @@
-from abc import ABC
-from abc import abstractmethod
-from typing import Any
+"""
+Testing database base class.
+
+Copyright 2025 (C) Nicholas M. Synovic
+"""
+
+from abc import ABC, abstractmethod
+import scoda.datasets as scoda_datasets
 
 
 class DB(ABC):
+    """
+    Abstract base class for testing database interactions.
+
+    This class defines the interface for database operations such as creation,
+    recreation, and data uploads. It serves as a blueprint for concrete database
+    implementations.
+
+    Attributes:
+        uri (str): The URI for connecting to the database.
+        username (str): The username for database authentication.
+        password (str): The password for database authentication.
+        database (str): The name of the database, defaulting to "research".
+
+    """
+
     def __init__(
         self,
         uri: str,
         username: str,
         password: str,
         database: str = "research",
-    ):
+    ) -> None:
+        """
+        Initialize the DB instance with connection parameters.
+
+        Args:
+            uri (str): The URI for connecting to the database.
+            username (str): The username for database authentication.
+            password (str): The password for database authentication.
+            database (str): The name of the database, defaulting to "research".
+
+        """
         self.uri: str = uri
         self.username: str = username
         self.password: str = password
         self.database: str = database
 
     @abstractmethod
-    def create(self) -> None: ...
+    def create(self) -> None:
+        """
+        Create database tables.
+
+        This method should be implemented by subclasses to define how tables
+        are created in the database.
+
+        """
+        ...
 
     @abstractmethod
-    def recreate(self) -> None: ...
+    def recreate(self) -> None:
+        """
+        Delete tables and recreate them.
+
+        This method should be implemented by subclasses to define how tables
+        are deleted and recreated in the database.
+
+        """
+        ...
 
     @abstractmethod
-    def batch_upload(self, data: Any) -> None: ...
+    def batch_upload(self, data: scoda_datasets.Dataset) -> None:
+        """
+        Upload all documents in one pass.
+
+        This method should be implemented by subclasses to define how data is
+        uploaded in a batch to the database.
+
+        Args:
+            data (scoda_datasets.Dataset): The data to be uploaded.
+
+        """
+        ...
 
     @abstractmethod
-    def sequential_upload(self, data: Any) -> None: ...
+    def sequential_upload(self, data: scoda_datasets.Dataset) -> None:
+        """
+        Upload documents one by one.
+
+        This method should be implemented by subclasses to define how data is
+        uploaded sequentially to the database.
+
+        Args:
+            data (scoda_datasets.Dataset): The data to be uploaded.
+
+        """
+        ...

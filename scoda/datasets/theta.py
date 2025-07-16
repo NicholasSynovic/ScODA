9ab5f95f5eaaa554eabb906d45dc6c0a5e661ebe
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-16 09:45:18 -0500

    Add docstrings

--- scoda/datasets/theta.py
+++ scoda/datasets/theta.py
@@ -8,15 +8,33 @@ Copyright 2025 (C) Nicholas M. Synovic
 from pathlib import Path
 from collections.abc import Iterator
 import scoda.datasets as scoda_datasets
-import os
 
 
 def load_theta_datasets(directory: Path) -> Iterator[scoda_datasets.Dataset]:
-    fps: list[Path] = [Path(directory, fp) for fp in os.listdir(path=directory)]
+    """
+    Load Theta datasets from the specified directory.
 
-    fp: Path
+    This function iterates over all files in the specified directory, creating a
+    Dataset instance for each file. If any path in the directory is not a file,
+    the function raises a FileNotFoundError.
+
+    Args:
+        directory (Path): The directory containing the dataset files.
+
+    Yields:
+        scoda_datasets.Dataset: An instance of Dataset for each file in the
+            directory.
+
+    Raises:
+        FileNotFoundError: If any path in the directory is not a file.
+
+    """
+    fps: list[Path] = [Path(directory, fp) for fp in Path(directory).iterdir()]
+
     for fp in fps:
-        if fp.is_file() == False:
-            return False
+        if not fp.is_file():
+            error_message: str = f"Path {fp} is not a file."
+            raise FileNotFoundError(error_message)
+
     for fp in fps:
         yield scoda_datasets.Dataset(name=fp.stem, fp=fp)

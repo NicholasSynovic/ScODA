bbb506cd75b6a5cbd7f0a3a5ec4648d380204cd1
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-16 09:19:15 -0500

    Add docstrings

--- scoda/main.py
+++ scoda/main.py
@@ -1,3 +1,10 @@
+"""
+Main entrypoint to ScODA command line application.
+
+Copyright 2025 (C) Nicholas M. Synovic
+
+"""
+
 import scoda.db.relational.last as last_rdbms
 import scoda.db.document.last as last_doc_db
 import scoda.db.results as scoda_results
@@ -11,16 +18,35 @@ import scoda.api.benchmarks as scoda_benchmarks
 
 
 def identify_input(key: str) -> bool:
-    # Returns True if LAST dataset, False if Theta
+    """
+    Identify the dataset type based on the input key.
+
+    Args:
+        key (str): The key indicating the dataset type, expected to be in the
+            format "dataset_name.some_other_info".
+
+    Returns:
+        bool: Returns True if the dataset is LAST, False if it is THETA.
+
+    """
     split_key: list[str] = key.split(sep=".")
 
-    if split_key[0] == "last":
-        return True
-    else:
-        return False
+    return split_key[0] == "last"
+    return False
 
 
-def create_last_db(db_name) -> last_rdbms.LAST | last_doc_db.LAST:
+def create_last_db(db_name: str) -> last_rdbms.LAST | last_doc_db.LAST:  # noqa: PLR0911
+    """
+    Create a database connection on the specified database name.
+
+    Args:
+        db_name (str): The name of the database to connect to.
+
+    Returns:
+        last_rdbms.LAST | last_doc_db.LAST: An instance of the appropriate
+            database connection class.
+
+    """
     match db_name:
         case "couch-db":
             return last_doc_db.CouchDB()
@@ -37,7 +63,11 @@ def create_last_db(db_name) -> last_rdbms.LAST | last_doc_db.LAST:
         case "sqlite3-memory":
             return last_rdbms.InMemorySQLite3()
         case _:
-            return last_rdbms.LAST(uri="", username="", password="")
+            return last_rdbms.LAST(
+                uri="",
+                username="",
+                password="",
+            )  # nosec: B106
 
 
 def run_benchmarks(
@@ -46,6 +76,21 @@ def run_benchmarks(
     datasets: list[scoda_dataset.Dataset],
     iterations: int,
 ) -> None:
+    """
+    Run benchmarking tests on the specified database using the provided datasets.
+
+    Performs various benchmarking tests, including batch and sequential writes.
+
+    Args:
+        test_db (scoda_db.DB): The database to be tested.
+        results_db (scoda_results.Results): The database to store benchmark
+            results.
+        datasets (list[scoda_dataset.Dataset]): The datasets to be used for
+            benchmarking.
+        iterations (int): The number of iterations to perform for each benchmark
+            test.
+
+    """
     scoda_benchmarks.benchmark_total_time_to_batch_write_tables(
         test_db=test_db,
         iterations=iterations,
@@ -76,12 +121,24 @@ def run_benchmarks(
 
 
 def main() -> int:
+    """
+    Execute the benchmarking process.
+
+    Sets up the CLI, identifies the dataset, creates database connections, loads
+        datasets, and runs benchmarks.
+
+    Returns:
+        int: Status code indicating the result of the execution. Returns 0 for
+            success, 1 if Theta dataset is not implemented, 2 if database
+            connection fails, and 3 if dataset loading fails.
+
+    """
     cli: CLI = CLI()
     args: dict[str, Any] = cli.parse_args().__dict__
     arg_keys: list[str] = list(args.keys())
 
     # True if LAST, False if Theta
-    print("Identifying dataset...")
+    print("Identifying dataset...")  # noqa: T201
     dataset: bool = identify_input(key=arg_keys[0])
 
     # TODO: Implement `theta` benchmarking
@@ -89,20 +146,20 @@ def main() -> int:
         return 1
 
     # Create db connection
-    print("Creating testing database connection...")
+    print("Creating testing database connection...")  # noqa: T201
     test_db: last_rdbms.LAST | last_doc_db.LAST
     test_db = create_last_db(db_name=args["db"][0])
-    if test_db.uri == "":
+    if test_db.uri is False:
         return 2
 
     # Create results db connection
-    print("Creating benchmarking results database...")
+    print("Creating benchmarking results database...")  # noqa: T201
     results_db: scoda_results.Results = scoda_results.Results(
         fp=args["output"][0],
     )
 
     # Load datasets
-    print("Loading datasets...")
+    print("Loading datasets...")  # noqa: T201
     datasets: list[scoda_dataset.Dataset] | bool
     # TODO: Add support for loading `theta` datasets
     datasets = scoda_dataset.load_llnl_datasets(directory=args["last.input"][0])
@@ -110,7 +167,7 @@ def main() -> int:
         return 3
 
     # Run benchmarks
-    print("Running benchmarks...")
+    print("Running benchmarks...")  # noqa: T201
     run_benchmarks(
         test_db=test_db,
         results_db=results_db,

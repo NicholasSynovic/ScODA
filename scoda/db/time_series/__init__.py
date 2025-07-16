e4e7256b3201939b7c95cc5ea55ab8fdefb51697
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-16 09:08:36 -0500

    Start work on time series databases

---
+++ scoda/db/time_series/__init__.py
@@ -0,0 +1,28 @@
+import scoda.db as scoda_db
+from abc import abstractmethod
+import scoda.api.dataset as scoda_dataset
+
+
+class TimeSeriesDB(scoda_db.DB):
+    def __init__(
+        self,
+        uri: str,
+        username: str,
+        password: str,
+        database: str = "research",
+    ):
+        super().__init__(uri, username, password, database)
+
+        self.create()
+
+    @abstractmethod
+    def create(self) -> None: ...
+
+    @abstractmethod
+    def recreate(self) -> None: ...
+
+    @abstractmethod
+    def batch_upload(self, data: scoda_dataset.Dataset) -> None: ...
+
+    @abstractmethod
+    def sequential_upload(self, data: scoda_dataset.Dataset) -> None: ...

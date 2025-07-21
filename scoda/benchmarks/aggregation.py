0b0e668531046a5a76e33da4009186ecea67dcd9
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-21 13:01:27 -0500

    Add method definitions

--- scoda/benchmarks/aggregation.py
+++ scoda/benchmarks/aggregation.py
@@ -4,3 +4,18 @@ Data aggregation benchmarks.
 Copyright 2025 (C) Nicholas M. Synovic
 
 """
+
+
+def query_min_value() -> None: ...  # noqa: D103
+
+
+def query_max_value() -> None: ...  # noqa: D103
+
+
+def query_average_value() -> None: ...  # noqa: D103
+
+
+def query_median_value() -> None: ...  # noqa: D103
+
+
+def query_groupby_time_window() -> None: ...  # noqa: D103

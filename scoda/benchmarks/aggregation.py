a9de88db968519344fdd77ee608c11e2e00ee9ca
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-21 13:05:18 -0500

    Add simple logic to be iterated upon later

--- scoda/benchmarks/aggregation.py
+++ scoda/benchmarks/aggregation.py
@@ -5,17 +5,187 @@ Copyright 2025 (C) Nicholas M. Synovic
 
 """
 
+from collections import defaultdict
+from time import time
 
-def query_min_value() -> None: ...  # noqa: D103
+from pandas import DataFrame
+from progress.bar import Bar
 
-
-def query_max_value() -> None: ...  # noqa: D103
-
-
-def query_average_value() -> None: ...  # noqa: D103
-
-
-def query_median_value() -> None: ...  # noqa: D103
+import scoda.datasets as scoda_dataset
+import scoda.db as scoda_db
+import scoda.db.results as scoda_results
 
 
-def query_groupby_time_window() -> None: ...  # noqa: D103
+def query_min_value(
+    test_db: scoda_db.DB,
+    iterations: int,
+    results_db: scoda_results.Results,
+    datasets: list[scoda_dataset.Dataset],
+) -> None:
+    # FIXME: Make this benchmark functional
+    data = defaultdict(list)
+
+    def _run() -> None:
+        ds: scoda_dataset.Dataset
+        for ds in datasets:
+            test_db.batch_read(table_name=ds.name)
+
+    with Bar(
+        "Benchmarking reading all tables from the database...",
+        max=iterations,
+    ) as bar:
+        for _ in range(iterations):
+            start_time: float = time()
+            _run()
+            end_time: float = time()
+            data["seconds"].append(end_time - start_time)
+            test_db.recreate()
+            bar.next()
+
+    df: DataFrame = DataFrame(data=data)
+    df.to_sql(
+        name="benchmark_total_time_to_batch_read_tables",
+        con=results_db.engine,
+        if_exists="append",
+        index=False,
+    )
+
+
+def query_max_value(
+    test_db: scoda_db.DB,
+    iterations: int,
+    results_db: scoda_results.Results,
+    datasets: list[scoda_dataset.Dataset],
+) -> None:
+    # FIXME: Make this benchmark functional
+    data = defaultdict(list)
+
+    def _run() -> None:
+        ds: scoda_dataset.Dataset
+        for ds in datasets:
+            test_db.batch_read(table_name=ds.name)
+
+    with Bar(
+        "Benchmarking reading all tables from the database...",
+        max=iterations,
+    ) as bar:
+        for _ in range(iterations):
+            start_time: float = time()
+            _run()
+            end_time: float = time()
+            data["seconds"].append(end_time - start_time)
+            test_db.recreate()
+            bar.next()
+
+    df: DataFrame = DataFrame(data=data)
+    df.to_sql(
+        name="benchmark_total_time_to_batch_read_tables",
+        con=results_db.engine,
+        if_exists="append",
+        index=False,
+    )
+
+
+def query_average_value(
+    test_db: scoda_db.DB,
+    iterations: int,
+    results_db: scoda_results.Results,
+    datasets: list[scoda_dataset.Dataset],
+) -> None:
+    # FIXME: Make this benchmark functional
+    data = defaultdict(list)
+
+    def _run() -> None:
+        ds: scoda_dataset.Dataset
+        for ds in datasets:
+            test_db.batch_read(table_name=ds.name)
+
+    with Bar(
+        "Benchmarking reading all tables from the database...",
+        max=iterations,
+    ) as bar:
+        for _ in range(iterations):
+            start_time: float = time()
+            _run()
+            end_time: float = time()
+            data["seconds"].append(end_time - start_time)
+            test_db.recreate()
+            bar.next()
+
+    df: DataFrame = DataFrame(data=data)
+    df.to_sql(
+        name="benchmark_total_time_to_batch_read_tables",
+        con=results_db.engine,
+        if_exists="append",
+        index=False,
+    )
+
+
+def query_median_value(
+    test_db: scoda_db.DB,
+    iterations: int,
+    results_db: scoda_results.Results,
+    datasets: list[scoda_dataset.Dataset],
+) -> None:
+    # FIXME: Make this benchmark functional
+    data = defaultdict(list)
+
+    def _run() -> None:
+        ds: scoda_dataset.Dataset
+        for ds in datasets:
+            test_db.batch_read(table_name=ds.name)
+
+    with Bar(
+        "Benchmarking reading all tables from the database...",
+        max=iterations,
+    ) as bar:
+        for _ in range(iterations):
+            start_time: float = time()
+            _run()
+            end_time: float = time()
+            data["seconds"].append(end_time - start_time)
+            test_db.recreate()
+            bar.next()
+
+    df: DataFrame = DataFrame(data=data)
+    df.to_sql(
+        name="benchmark_total_time_to_batch_read_tables",
+        con=results_db.engine,
+        if_exists="append",
+        index=False,
+    )
+
+
+def query_groupby_time_window(
+    test_db: scoda_db.DB,
+    iterations: int,
+    results_db: scoda_results.Results,
+    datasets: list[scoda_dataset.Dataset],
+) -> None:
+    # FIXME: Make this benchmark functional
+    data = defaultdict(list)
+
+    def _run() -> None:
+        ds: scoda_dataset.Dataset
+        for ds in datasets:
+            test_db.batch_read(table_name=ds.name)
+
+    with Bar(
+        "Benchmarking reading all tables from the database...",
+        max=iterations,
+    ) as bar:
+        for _ in range(iterations):
+            start_time: float = time()
+            _run()
+            end_time: float = time()
+            data["seconds"].append(end_time - start_time)
+            test_db.recreate()
+            bar.next()
+
+    df: DataFrame = DataFrame(data=data)
+    df.to_sql(
+        name="benchmark_total_time_to_batch_read_tables",
+        con=results_db.engine,
+        if_exists="append",
+        index=False,
+    )

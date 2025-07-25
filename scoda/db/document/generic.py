60a92a59723059fd0eb0c67e1c23cf16fd9fd7e5
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-25 09:10:12 -0500

    Delete the old document database implementations

---
+++ scoda/db/document/generic.py
@@ -0,0 +1,53 @@
+from abc import abstractmethod
+
+import scoda.datasets.generic
+import scoda.db
+
+
+class DocumentDB(scoda.db.DB):
+    def __init__(
+        self,
+        convert_time_column_to_int: bool = True,
+    ) -> None:
+        super().__init__(convert_time_column_to_int=convert_time_column_to_int)
+
+    @abstractmethod
+    def batch_upload(self, dataset: scoda.datasets.generic.Dataset) -> None: ...
+
+    @abstractmethod
+    def batch_read(self, table_name: str) -> None: ...
+
+    @abstractmethod
+    def create(self) -> None: ...
+
+    @abstractmethod
+    def delete(self) -> None: ...
+
+    @abstractmethod
+    def query_average_value(
+        self,
+        table_name: str,
+        column_name: str,
+    ) -> None: ...
+
+    @abstractmethod
+    def query_groupby_time_window_value(
+        self,
+        table_name: str,
+        column_name: str,
+    ) -> None: ...
+
+    @abstractmethod
+    def query_max_value(self, table_name: str, column_name: str) -> None: ...
+
+    @abstractmethod
+    def query_min_value(self, table_name: str, column_name: str) -> None: ...
+
+    @abstractmethod
+    def query_mode_value(self, table_name: str, column_name: str) -> None: ...
+
+    @abstractmethod
+    def sequential_read(self, table_name: str, rows: int) -> None: ...
+
+    @abstractmethod
+    def sequential_upload(self, dataset: scoda.datasets.generic.Dataset) -> None: ...
